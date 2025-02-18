964c4e291993d9e961e48f318f452edce00e8413
Goober333 <sheltonsjoseph@gmail.com>
2025-02-18 18:07:59 -0500

    adding possession files to Scripts

---
+++ Dead of Knight/Assets/Scripts/Possession.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Possession : MonoBehaviour
+{
+    //All objects that can be possessed must have label Possessable
+    GameObject[] possessables; 
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        possessables = GameObject.FindGameObjectsWithTag("Possessable");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        possess();
+    }
+
+    void possess()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            foreach (GameObject possessObject in possessables)
+            {
+                if ((transform.position - possessObject.transform.position).magnitude <= 2)
+                {
+                    spriteRenderer.enabled = false;
+                    Debug.Log("Possessing");
+                    possessObject.tag = "Player";
+                    possessObject.GetComponent<Possessable>().enabled = true;
+                    return;
+                }
+            }
+        }
+    }
+}
